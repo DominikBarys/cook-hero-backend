f2df0bee8ff0324ff2c9638df390a8abdfb2bf10
Brystol <odido160@gmail.com>
2023-10-13 15:31:30 +0200

    Init commit, artifacts of eureka and gateway modules

---
+++ gateway/src/test/java/com/barysdominik/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barysdominik.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
