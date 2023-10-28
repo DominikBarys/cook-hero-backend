c1bcb747ea92fc880dc35fd12ba33b6444424894
Brystol <odido160@gmail.com>
2023-10-28 18:45:21 +0200

    Configured notification service in gateway

---
+++ gateway/src/main/java/com/barysdominik/gateway/entity/endpoint/NotificationServiceEndpoints.java
@@ -0,0 +1,32 @@
+package com.barysdominik.gateway.entity.endpoint;
+
+import com.barysdominik.gateway.configuration.RouteValidator;
+import com.barysdominik.gateway.entity.Endpoint;
+import com.barysdominik.gateway.entity.HttpMethod;
+import com.barysdominik.gateway.entity.Role;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationServiceEndpoints {
+
+    private final RouteValidator routeValidator;
+
+    private List<Endpoint> endpointList = new ArrayList<>(List.of(
+            new Endpoint("/api/v1/notification/all", HttpMethod.GET, Role.GUEST),
+            new Endpoint("/api/v1/notification", HttpMethod.GET, Role.GUEST),
+            new Endpoint("/api/v1/notification/all", HttpMethod.DELETE, Role.GUEST),
+            new Endpoint("/api/v1/notification", HttpMethod.DELETE, Role.GUEST)
+    ));
+
+    @PostConstruct
+    public void registerEndpoints(){
+        routeValidator.addEndpoints(endpointList);
+    }
+
+}
